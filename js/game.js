f20da2c0e74269d4be4fb52c4a8aec078813d337
XonGado <cjubs.delgado@gmail.com>
2017-04-28 16:29:40 +0800

    Setup.js is now available.

--- js/game.js
+++ js/game.js
@@ -1,34 +1,14 @@
-var cd1, cd2, cd3, cd4, cd5, cd6, cd7, cd8, cd9;
-var gold_leaves = 50;
-var waves = 0;
-var kills = 0;
-var score = 0;
-var endGame = false;
-
-
-
 //changes game.js was changed to game-clyde.js
 
 window.onload = function () {
     //    testCreaturesCreate();
     //    testMonstersCreate();
-    creatures = [];
+    createCards();
 
-	creatures.push(new Creature("normal creature", 3000, 100));
-	creatures.push(new Creature("normal creature", 3000, 100));
-	creatures.push(new Creature("normal creature", 3000, 100));
-	creatures.push(new Creature("normal creature", 3000, 100));
-	creatures.push(new Creature("normal creature", 3000, 100));
-	creatures.push(new Creature("normal creature", 3000, 100));
-	creatures.push(new Creature("normal creature", 3000, 100));
-	creatures.push(new Creature("normal creature", 3000, 100));
-	creatures.push(new Creature("normal creature", 3000, 100));
-	creatures.push(new Creature("normal creature", 3000, 100));
-
-    unaffordable(0);
+    for (var i = creatures_card.length - 1; i >= 0; i--) {
+    	unlock(i);
+    }
 }
-
-
 
 function testCreaturesCreate() {
     var normalCreature1;
@@ -61,92 +41,8 @@ function testMonstersCreate() {
 
 // Merged JS Until the end.
 
-function CreatureCard(name, cooldown, cost, locked) {
-    this.name = name;
-    this.cooldown = cooldown;
-    this.cost = cost;
-    this.locked = locked;
-}
 
-function unaffordable(index) {
-    if (!creatures[index].locked) {
-        creature_cards = document.getElementsByClassName("creature-card");
-        creature_cards[index].children[0].style.display = "flex";
-    }
-}
 
-function unlock(index) {
-    creature_cards = document.getElementsByClassName("creature-card");
-    creature_cards[index].children[1].style.display = "none";
-    return false;
-}
 
-function setCooldown(index, locked, cooldown) {
-    if (!locked) {
-        creature_cards = document.getElementsByClassName("creature-card");
-        creature_cards[index].children[2].style.display = "flex";
-        cooldown_div = document.getElementsByClassName("cooldown");
-        var time = cooldown_div[index].children[0];
-        console.log(cooldown);
-        time.innerHTML = (cooldown / 1000);
-        console.log(time.innerHTML);
-    }
-}
 
-function cooldownTick(index) {
-    cooldown = document.getElementsByClassName("cooldown");
-    var time = parseInt(cooldown[index].children[0].innerHTML);
-    console.log(time);
 
-    if (time) {
-        time--;
-        time.innerHTML = time;
-        cooldown[index].children[0].innerHTML = time;
-    } else {
-        var id = "creature_" + index + "_cd";
-        creature_cards = document.getElementsByClassName("creature-card");
-        creature_cards[index].children[2].style.display = "none";
-        if (index == 0) {
-            clearTimeout(creature_0_cd);
-        } else if (index == 1) {
-            clearTimeout(cd1);
-        } else if (index == 2) {
-            clearTimeout(cd2);
-        } else if (index == 3) {
-            clearTimeout(cd3);
-        } else if (index == 4) {
-            clearTimeout(cd4);
-        } else if (index == 5) {
-            clearTimeout(cd5);
-        } else if (index == 6) {
-            clearTimeout(cd6);
-        } else if (index == 7) {
-            clearTimeout(cd7);
-        } else if (index == 8) {
-            clearTimeout(cd8);
-        } else if (index == 9) {
-            clearTimeout(cd9);
-        }
-    }
-}
-
-function kill() {
-    kills++;
-    document.getElementById("kill-count").innerHTML = kills;
-}
-
-function nextWave() {
-    // Generate Zombies here. Maybe?
-    waves++;
-    document.getElementById("wave-count").innerHTML = kills;
-}
-
-function scoreUp(points) {
-    score += points;
-    document.getElementById("score-count").innerHTML = score;
-}
-
-function goldLeafModify(leaves) {
-    gold_leaves += leaves;
-    document.getElementById("gold-leaf-count").innerHTML = gold_leaves;
-}
